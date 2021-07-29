c2115be4cc00d6fcff3bc193c46e1e31b7fc143a
pranjalagrawal2012 <pranjalagrawal2012@gmail.com>
2021-07-29 13:41:13 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [data, setdata] = useState(0)
+  
+  function update(i){
+    if(i==="i"){
+      setdata(data+1);
+    }else if(i==='d'){
+        setdata(data-1);
+    }
+    else{
+      setdata(0);
+    }
+  
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <h1 class ="navbar">{data}</h1>
+      <button onClick={()=>update('d')}>Decrement</button>
+      <button onClick={update}>Reset</button> 
+      <button onClick={()=>update('i')}>Increment</button>
     </div>
   );
 }
